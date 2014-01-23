96d4379fc8f66973470f0188274bee83571d4650
Adelbert Chang <adelbertc@gmail.com>
2014-01-22 19:34:36 -0800

    Organize IndexedSet
    * Remove random selection - this should be from the user
    * Add elementAt function for random access
    * Organize functions

--- src/main/scala/nets/IndexedSet.scala
+++ src/main/scala/nets/IndexedSet.scala
@@ -1,7 +1,5 @@
 package nets
 
-import com.nicta.rng.Rng
-
 import scala.annotation.tailrec
 import scala.collection.immutable.Seq
 
@@ -10,56 +8,14 @@ import scalaz.syntax.order._
 
 /** Set that supports O(logn) insert for O(1) random access */
 final class IndexedSet[A] private(private val repr: Vector[A]) {
-  def insert(x: A)(implicit A: Order[A]): IndexedSet[A] = {
-    val (membership, index) = IndexedSet.binarySearch(repr, x)
-    if (membership) this
-    else new IndexedSet(IndexedSet.insertAt(repr, x, index))
-  }
-
-  def indexOf(x: A)(implicit A: Order[A]): Option[Int] = {
-    val (elemIsThere, index) = IndexedSet.binarySearch(repr, x)
-    if (elemIsThere) Some(index) else None
-  }
-
   def contains(x: A)(implicit A: Order[A]): Boolean =
     member(x)
-
-  def member(x: A)(implicit A: Order[A]): Boolean =
-    indexOf(x).isDefined
-
-  def notMember(x: A)(implicit A: Order[A]): Boolean =
-    !member(x)
-
-  def isSubsetOf(other: IndexedSet[A])(implicit A: Equal[A]): Boolean = {
-    @tailrec
-    def isSubsetOfAux(xs: Vector[A], ys: Vector[A]): Boolean =
-      if (xs.isEmpty) true
-      else if (ys.isEmpty) false
-      else if (xs.head === ys.head) isSubsetOfAux(xs.tail, ys.tail)
-      else false
-
-    (this.size <= other.size) && isSubsetOfAux(repr, other.repr)
-  }
-
-  def isProperSubsetOf(other: IndexedSet[A])(implicit A: Equal[A]): Boolean =
-    (this.size < other.size) && isSubsetOf(other)
 
   def delete(x: A)(implicit A: Order[A]): IndexedSet[A] = {
     val (membership, n) = IndexedSet.binarySearch(repr, x)
     if (membership) new IndexedSet(repr.take(n) ++ repr.drop(n + 1))
     else this
   }
-
-  def foldLeft[B](z: B)(f: (B, A) => B): B =
-    repr.foldLeft(z)(f)
-
-  def foldRight[B](z: B)(f: (A, B) => B): B =
-    repr.foldRight(z)(f)
-
-  def union(other: IndexedSet[A])(implicit A: Order[A]): IndexedSet[A] =
-    other.foldLeft(this) { (set, x) =>
-      set.insert(x)
-    }
 
   def difference(other: IndexedSet[A])(implicit A: Order[A]): IndexedSet[A] = {
     val tempSet =
@@ -74,6 +30,30 @@ final class IndexedSet[A] private(private val repr: Vector[A]) {
     }
   }
 
+  def elementAt(i: Int): Option[A] =
+    try { Option(repr(i)) }
+    catch { case _: Throwable => None }
+
+  def filter(p: A => Boolean): IndexedSet[A] =
+    new IndexedSet(repr.filter(p))
+
+  def foldLeft[B](z: B)(f: (B, A) => B): B =
+    repr.foldLeft(z)(f)
+
+  def foldRight[B](z: B)(f: (A, B) => B): B =
+    repr.foldRight(z)(f)
+
+  def indexOf(x: A)(implicit A: Order[A]): Option[Int] = {
+    val (elemIsThere, index) = IndexedSet.binarySearch(repr, x)
+    if (elemIsThere) Some(index) else None
+  }
+
+  def insert(x: A)(implicit A: Order[A]): IndexedSet[A] = {
+    val (membership, index) = IndexedSet.binarySearch(repr, x)
+    if (membership) this
+    else new IndexedSet(IndexedSet.insertAt(repr, x, index))
+  }
+
   def intersection(other: IndexedSet[A])(implicit A: Order[A]): IndexedSet[A] = {
     this.foldLeft(IndexedSet.empty[A]) { (set, x) =>
       if (other.member(x)) set.insert(x)
@@ -81,40 +61,56 @@ final class IndexedSet[A] private(private val repr: Vector[A]) {
     }
   }
 
-  def randomElement: Rng[Option[A]] =
-    Rng.int.map { i =>
-      try { Some(repr(i)) }
-      catch { case _: Throwable => None }
-    }
+  def isProperSubsetOf(other: IndexedSet[A])(implicit A: Equal[A]): Boolean =
+    (this.size < other.size) && isSubsetOf(other)
 
-  def filter(p: A => Boolean): IndexedSet[A] =
-    new IndexedSet(repr.filter(p))
+  def isSubsetOf(other: IndexedSet[A])(implicit A: Equal[A]): Boolean = {
+    @tailrec
+    def isSubsetOfAux(xs: Vector[A], ys: Vector[A]): Boolean =
+      if (xs.isEmpty) true
+      else if (ys.isEmpty) false
+      else if (xs.head === ys.head) isSubsetOfAux(xs.tail, ys.tail)
+      else false
+
+    (this.size <= other.size) && isSubsetOfAux(repr, other.repr)
+  }
+
+  def maxOption: Option[A] = repr.lastOption
+
+  def member(x: A)(implicit A: Order[A]): Boolean =
+    indexOf(x).isDefined
+
+  def minOption: Option[A] = repr.headOption
+
+  def notMember(x: A)(implicit A: Order[A]): Boolean =
+    !member(x)
 
   def partition(p: A => Boolean): (IndexedSet[A], IndexedSet[A]) = {
     val (l, r) = repr.partition(p)
     (new IndexedSet(l), new IndexedSet(r))
   }
 
-  def minOption: Option[A] = repr.headOption
-
-  def maxOption: Option[A] = repr.lastOption
-
   val size: Int = repr.size
 
   def toList: List[A] = repr.toList
 
   val toVector: Vector[A] = repr
+
+  def union(other: IndexedSet[A])(implicit A: Order[A]): IndexedSet[A] =
+    other.foldLeft(this) { (set, x) =>
+      set.insert(x)
+    }
 }
 
 object IndexedSet {
   def empty[A : Order]: IndexedSet[A] =
     new IndexedSet(Vector.empty)
 
-  def singleton[A : Order](x: A): IndexedSet[A] =
-    new IndexedSet(Vector(x))
-
   def fromSeq[A : Order](xs: Seq[A]): IndexedSet[A] =
     xs.foldLeft(IndexedSet.empty[A])((xs, x) => xs.insert(x))
+
+  def singleton[A : Order](x: A): IndexedSet[A] =
+    new IndexedSet(Vector(x))
 
   private def insertAt[A](xs: Vector[A], x: A, n: Int): Vector[A] =
     xs.take(n) ++ Vector(x) ++ xs.drop(n)
