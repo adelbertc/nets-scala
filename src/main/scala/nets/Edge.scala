b63c386970aa63e6ef58857040efbf9c0de6aabb
Adelbert Chang <adelbertc@gmail.com>
2014-01-25 18:34:21 -0800

    re-add graph constructors

--- src/main/scala/nets/Edge.scala
+++ src/main/scala/nets/Edge.scala
@@ -4,13 +4,15 @@ import scalaz.{ Order, Ordering }
 
 import spire.algebra.{ Rig }
 
-final class Edge[A, W] private(val from: A, val to: A, val weight: W)
+final class Edge[A, W] private(val from: A, val to: A, val weight: W) {
+  def reverse(implicit W: Rig[W]): Edge[A, W] = Edge.weighted(to, from, weight)
+}
 
 object Edge extends EdgeInstances {
   def unweighted[A, W](from: A, to: A)(implicit W: Rig[W]): Edge[A, W] =
     new Edge(from, to, W.one)
 
-  def weighted[A, W : Rig](from: A, to: A, weight: W): Edge[A, W] =
+  def weighted[A, W](from: A, to: A, weight: W)(implicit W: Rig[W]): Edge[A, W] =
     new Edge(from, to, weight)
 }
 
--- src/main/scala/nets/Graph.scala
+++ src/main/scala/nets/Graph.scala
@@ -3,23 +3,29 @@ package nets
 import scala.collection.immutable.Seq
 
 import scalaz.{ ==>>, Order }
+import scalaz.syntax.order._
+
+import spire.algebra.{ Rig }
 
 final class Graph[A, W] private(
-    private val adjacencyList: A ==>> IndexedSet[Edge[A, W]],
+    private val adjList: A ==>> IndexedSet[Edge[A, W]],
     val isDirected: Boolean) {
 
   /* Vertex-centric functions */
-  def degree(node: A)(implicit A: Order[A]): Option[Int] = adjacencyList.lookup(node).map(_.size)
-  def nodes: List[A] = adjacencyList.keys
-  def order: Int = adjacencyList.size
+  def degree(node: A)(implicit A: Order[A]): Option[Int] =
+    adjList.lookup(node).map(_.size)
+  def neighbors(node: A)(implicit A: Order[A]): Option[IndexedSet[Edge[A, W]]] =
+    adjList.lookup(node)
+  def nodes: List[A] = adjList.keys
+  def order: Int = adjList.size
   def vertices: List[A] = nodes
 
   /* Edge-centric functions */
   def edges: List[Edge[A, W]] = 
-    adjacencyList.toList.flatMap { case (_, nbors) => nbors.toList }
+    adjList.toList.flatMap { case (_, nbors) => nbors.toList }
 
   def size: Int = {
-    val bidirectionalEdges = adjacencyList.fold(0)((_, ns, acc) => acc + ns.size)
+    val bidirectionalEdges = adjList.fold(0)((_, ns, acc) => acc + ns.size)
     if (isDirected) bidirectionalEdges else bidirectionalEdges / 2
   }
 
@@ -27,4 +33,37 @@ final class Graph[A, W] private(
 }
 
 object Graph {
+  def fromDirectedEdges[A : Order, W](es: Seq[Edge[A, W]]): Graph[A, W] = {
+    val adjacencyList =
+      es.foldLeft(==>>.empty[A, IndexedSet[Edge[A, W]]]) { (map, edge) =>
+        if (edge.from === edge.to)
+          map
+        else if (map.member(edge.from))
+          map.insert(edge.from, IndexedSet.singleton(edge))
+        else
+          map.adjust(edge.from, _.insert(edge))
+      }
+    new Graph(adjacencyList, true)
+  }
+
+  def fromUndirectedEdges[A : Order, W : Rig](es: Seq[Edge[A, W]]): Graph[A, W] = {
+    val adjacencyList =
+      es.foldLeft(==>>.empty[A, IndexedSet[Edge[A, W]]]) { (map, edge) =>
+        val tempMap =
+            if (edge.from === edge.to)
+              map
+            else if (map.member(edge.from))
+              map.insert(edge.from, IndexedSet.singleton(edge))
+            else
+              map.adjust(edge.from, _.insert(edge))
+
+        if (edge.from === edge.to)
+          map
+        else if (tempMap.member(edge.to))
+          tempMap.insert(edge.to, IndexedSet.singleton(edge.reverse))
+        else
+          tempMap.adjust(edge.to, _.insert(edge.reverse))
+      }
+    new Graph(adjacencyList, false)
+  }
 }
