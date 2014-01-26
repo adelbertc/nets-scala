52cf36065c999631e109e7058e0161e4b7d9cd07
Adelbert Chang <adelbertc@gmail.com>
2014-01-25 20:53:15 -0800

    add bfs traversal

--- src/main/scala/nets/Graph.scala
+++ src/main/scala/nets/Graph.scala
@@ -1,6 +1,6 @@
 package nets
 
-import scala.collection.immutable.Seq
+import scala.collection.immutable.{ Queue, Seq }
 
 import scalaz.{ ==>>, Order }
 import scalaz.syntax.order._
@@ -12,13 +12,13 @@ final class Graph[A, W] private(
     val isDirected: Boolean) {
 
   /* Vertex-centric functions */
-  def degree(node: A)(implicit A: Order[A]): Option[Int] =
-    adjList.lookup(node).map(_.size)
-  def neighbors(node: A)(implicit A: Order[A]): Option[IndexedSet[Edge[A, W]]] =
-    adjList.lookup(node)
-  def nodes: List[A] = adjList.keys
+  def degree(u: A)(implicit A: Order[A]): Option[Int] =
+    adjList.lookup(u).map(_.size)
+  def neighbors(u: A)(implicit A: Order[A]): Option[IndexedSet[Edge[A, W]]] =
+    adjList.lookup(u)
+  def nodes: List[A] = vertices
   def order: Int = adjList.size
-  def vertices: List[A] = nodes
+  def vertices: List[A] = adjList.keys
 
   /* Edge-centric functions */
   def edges: List[Edge[A, W]] = 
@@ -27,6 +27,28 @@ final class Graph[A, W] private(
   def size: Int = {
     val bidirectionalEdges = adjList.fold(0)((_, ns, acc) => acc + ns.size)
     if (isDirected) bidirectionalEdges else bidirectionalEdges / 2
+  }
+
+  def bfs(root: A)(implicit A: Order[A]): Option[A ==>> Int] = {
+    def bfsStep(map: A ==>> Int, queue: Queue[A], nbors: IndexedSet[Edge[A, W]], value: Int): (A ==>> Int, Queue[A]) =
+      nbors.foldRight((map, queue)) {
+        case (nbor, p@(m, q)) =>
+          if (map.member(nbor.to)) p
+          else (m.insert(nbor.to, value), q.enqueue(nbor.to))
+      }
+
+    def bfsAux(map: A ==>> Int, queue: Queue[A]): Option[A ==>> Int] =
+      if (queue.nonEmpty) {
+        val (u, rest) = queue.dequeue
+        for {
+          length <- map.lookup(u)
+          nbors  <- neighbors(u)
+          args   =  bfsStep(map, queue, nbors, length + 1)
+          m      <- bfsAux(args._1, args._2)
+        } yield m
+      } else Some(map)
+    
+    bfsAux(==>>.singleton(root, 0), Queue(root))
   }
 
   val isUndirected: Boolean = !isDirected
